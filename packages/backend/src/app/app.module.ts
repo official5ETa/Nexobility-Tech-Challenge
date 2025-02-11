47fd817ff1f61501ca2572bbcbeb3e472c4298c3
Clemens Banowski <officialeta09@gmail.com>
2025-02-11 14:48:16 +0100

    sourced out database module

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGODB_URI as string),
-  ],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ packages/backend/src/app/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
